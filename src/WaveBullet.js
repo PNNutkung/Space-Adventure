bc89486030b876cf67068d515c28a42bb522cac2
Pipatpol <pipatpol.t@ku.th>
2015-04-03 10:39:15 +0000

    remove random position

--- src/WaveBullet.js
+++ src/WaveBullet.js
@@ -4,10 +4,5 @@ var WaveBullet = Bullet.extend({
 
         this.step += 1;
         this.setPosition( cc.p( pos.x + 5, pos.y + 4 * Math.sin( Math.PI * this.step / this.period ) ) );
-    },
-
-    randomPosition: function() {
-        var y = 10 + Math.floor( Math.random() * ( screenHeight - 10 ) );
-        this.setPosition( new cc.Point( 100, y ) );
     }
 });
