7a6ea2c3a1db57e1d3d2e60ae8e7d8ae0f7717eb
Pipatpol <pipatpol.t@ku.th>
2015-04-03 11:06:18 +0000

    spread code to new method

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -3,10 +3,21 @@ var GameLayer = cc.LayerColor.extend({
         this._super( new cc.Color( 0, 0, 0, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
 
+        this.newPlayer();
+        this.newBullet();
+        this.newLife();
+
+        this.addKeyboardHandlers();
+        this.scheduleUpdate();
+        
+        return true;
+    },
+    newPlayer: function () {
         this.ship = new Ship();
         this.ship.setPosition( new cc.Point( 700, 300 ) );
         this.addChild( this.ship );
-
+    },
+    newBullet: function() {
         this.bullets = [];
         for ( var i = 0; i < 15; i++ ) {
             var p = Math.random();
@@ -18,31 +29,28 @@ var GameLayer = cc.LayerColor.extend({
             } else {
                 bullet = new WaveBullet();
             }
-            bullet.randomPosition();
-            bullet.setPositionX( 100 - 150 * i );
-            this.addChild( bullet );
-            bullet.scheduleUpdate();
-
+            this.randomBulletPosition( bullet , i );
             this.bullets.push( bullet );
         }
-
+    },
+    randomBulletPosition: function( bullet, i ) {
+        bullet.randomPosition();
+        bullet.setPositionX( 100 - 150 * i );
+        this.addChild( bullet );
+        bullet.scheduleUpdate();
+    },
+    newLife: function() {
         this.life = 10;
         this.lifeLabel = cc.LabelTTF.create( 10, 'Arial', 32 );
         this.lifeLabel.setPosition( cc.p( 700, 550 ) );
         this.addChild( this.lifeLabel );
-        
-        this.addKeyboardHandlers();
-        this.scheduleUpdate();
-        
-        return true;
     },
-
     update: function( dt ) {
         var self = this;
         this.bullets.forEach( function( bullet, i ) {
             var x = bullet.getPositionX();
             if ( self.isOutOfScreen( x ) ) {
-                self.isPlayer();
+                self.isPlayer( bullet );
             }
             if ( x > screenWidth ) {
                 bullet.randomPosition();
@@ -52,7 +60,7 @@ var GameLayer = cc.LayerColor.extend({
     isOutOfScreen: function( x ) {
         return ( x < screenWidth ) && ( x > screenWidth - 100 );
     },
-    isPlayer: function() {
+    isPlayer: function( bullet ) {
         var self = this;
         var y = bullet.getPositionY();
         if ( Math.abs( y - self.ship.getPositionY() ) < 25 ) {
@@ -68,9 +76,9 @@ var GameLayer = cc.LayerColor.extend({
     gameOver: function() {
         var self = this;
         var gameOverLabel = cc.LabelTTF.create( 'Game over', 'Arial', 60 );
-                gameOverLabel.setPosition( cc.p( 400, 300 ) );
-                self.addChild( gameOverLabel );
-                cc.director.pause();
+        gameOverLabel.setPosition( cc.p( 400, 300 ) );
+        self.addChild( gameOverLabel );
+        cc.director.pause();
     },
     onKeyDown: function( keyCode, event ) {
         if ( keyCode == cc.KEY.up ) {
